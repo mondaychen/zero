a3f425385fa3e4503f27d414cf737a43b5661c73
mondaychen <monday.chen@gmail.com>
2015-03-05 12:08:20 -0500

    add pager demo

--- client/app/main/components/contact_detail.js
+++ client/app/main/components/contact_detail.js
@@ -1,4 +1,7 @@
 angular.module('zeroApp').directive('contactDetail', function() {
+  var page = function(num) {
+    console.log(num)
+  }
   return {
     restrict: 'A', // for IE 8
     scope: {
@@ -8,7 +11,16 @@ angular.module('zeroApp').directive('contactDetail', function() {
     link: function (scope, element) {
       var content = scope.detail
       if (scope.type == 'email') {
-        content = '<a href="mailto:#">#</a>'.replace(/\#/g, scope.detail)
+        content = '<a href="mailto:@">@</a>'.replace(/\@/g, scope.detail)
+      }
+      if (scope.type == 'pagerNum') {
+        content = ('<a href="#">@</a>')
+          .replace(/\@/g, scope.detail)
+        content = $(content)
+        content.click(function(e) {
+          e.preventDefault()
+          page(scope.detail)
+        })
       }
       element.html(content)
     }
