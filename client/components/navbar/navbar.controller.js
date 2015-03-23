405c279a569c5ded26b34550a0c0ec67a86918eb
mondaychen <monday.chen@gmail.com>
2015-03-23 01:45:59 -0400

    add additional options for search form

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,22 +2,33 @@
 
 angular.module('zeroApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    // $scope.menu = [{
+    //   'title': 'Home',
+    //   'link': '/'
+    // }];
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+    // $scope.isCollapsed = true;
+    // $scope.isLoggedIn = Auth.isLoggedIn;
+    // $scope.isAdmin = Auth.isAdmin;
+    // $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
+    // $scope.logout = function() {
+    //   Auth.logout();
+    //   $location.path('/login');
+    // };
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
+    // $scope.isActive = function(route) {
+    //   return route === $location.path();
+    // };
+
+    $scope.searchOptions = {
+      institute: ['Cornell', 'Columbia'],
+      type: ['MRN', 'Accession #']
+    }
+
+    $scope.search = {
+      institute: 0,
+      type: 0,
+      showOptions: false
+    }
   });
