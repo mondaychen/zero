f45ef91dbb27966b2513e8512beb42681a1c752a
mondaychen <monday.chen@gmail.com>
2015-03-26 23:27:36 -0400

    remove dead code

--- client/app/main/components/info_collection.js
+++ client/app/main/components/info_collection.js
@@ -155,9 +155,6 @@ angular.module('zeroApp').factory('InfoCollection',
     
     if(!options.local) {
       params.provider_id = self.id
-      postVote(params, updateLocalStatus, function() {
-        item.voteStatus = item.oldVoteStatus
-      }, options.newAdded)
       postVote(params, {
         whenSuccess: updateLocalStatus,
         whenError: function() {
