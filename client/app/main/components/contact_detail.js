7fddaf0387393f484b80178985d32a31c755874a
mondaychen <monday.chen@gmail.com>
2015-04-19 10:42:00 -0400

    format phone numbers

--- client/app/main/components/contact_detail.js
+++ client/app/main/components/contact_detail.js
@@ -7,10 +7,20 @@ angular.module('zeroApp').directive('contactDetail', function() {
     },
     link: function (scope, element) {
       var content = scope.detail
-      if (scope.type == 'email') {
+      if (scope.type === 'officePhone' || scope.type === 'mobilePhone') {
+        // formatting
+        content = scope.detail.replace(/\D/g, '')
+        var PHONE_NUM_LENGTH = 10
+        var startPosition = content.length - PHONE_NUM_LENGTH
+        content = [content.slice(0, startPosition), '(',
+          content.slice(startPosition, startPosition + 3), ')',
+          content.slice(startPosition + 3, startPosition + 6), '-',
+          content.slice(startPosition + 6)].join('')
+      }
+      if (scope.type === 'email') {
         content = '<a href="mailto:@">@</a>'.replace(/\@/g, scope.detail)
       }
-      if (scope.type == 'pagerNum') {
+      if (scope.type === 'pagerNum') {
         content = ('<a href="javascript:;" data-toggle="modal" data-target="#pager-box"'
           + 'data-number="@">@</a>')
           .replace(/\@/g, scope.detail)
