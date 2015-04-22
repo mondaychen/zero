09ac26019a9928ab888a068f72de54a5e79706c4
mondaychen <monday.chen@gmail.com>
2015-04-22 19:04:42 -0400

    better formatting numbers

--- client/app/main/components/contact_detail.js
+++ client/app/main/components/contact_detail.js
@@ -2,6 +2,16 @@ angular.module('zeroApp').directive('contactDetail', function() {
   var msgLinkTmpl = _.template('<button class="btn btn-link" data-toggle="modal"'
     + ' data-target="#<%= type %>-box"'
     + ' data-number="<%= number %>"><%= text %></button>')
+  function numberFormatter (number) {
+    number = number.replace(/\D/g, '')
+    var PHONE_NUM_LENGTH = 10
+    var startPosition = number.length - PHONE_NUM_LENGTH
+    number = _.compact([number.slice(0, startPosition),
+      number.slice(startPosition, startPosition + 3),
+      number.slice(startPosition + 3, startPosition + 6),
+      number.slice(startPosition + 6)]).join('-')
+    return number
+  }
   return {
     restrict: 'A', // for IE 8
     scope: {
@@ -11,19 +21,14 @@ angular.module('zeroApp').directive('contactDetail', function() {
     link: function (scope, element) {
       var content = scope.detail
       if (scope.type === 'officePhone' || scope.type === 'mobilePhone') {
-        // formatting
-        var text = scope.detail.replace(/\D/g, '')
-        var PHONE_NUM_LENGTH = 10
-        var startPosition = text.length - PHONE_NUM_LENGTH
-        text = [text.slice(0, startPosition), '(',
-          text.slice(startPosition, startPosition + 3), ') ',
-          text.slice(startPosition + 3, startPosition + 6), '-',
-          text.slice(startPosition + 6)].join('')
         content = msgLinkTmpl({
           type: 'SMS',
           number: scope.detail,
-          text: text
+          text: numberFormatter(scope.detail)
         })
+      }
+      if (scope.type === 'faxNum') {
+        content = numberFormatter(scope.detail)
       }
       if (scope.type === 'email') {
         content = '<a href="mailto:@">@</a>'.replace(/\@/g, scope.detail)
