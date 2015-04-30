e06e492ff94b4121a6f7e98871b20bff13018b55
mondaychen <monday.chen@gmail.com>
2015-04-30 18:54:04 -0400

    bugfix: clipboard doesn't work on IE

--- client/app/main/components/clipboard_btn.js
+++ client/app/main/components/clipboard_btn.js
@@ -6,20 +6,39 @@ angular.module('zeroApp').directive('clipboardButton', function() {
     },
     link: function (scope, element) {
       var client = new ZeroClipboard(element)
-      client.on("copy", function (event) {
-        var clipboard = event.clipboardData;
-        clipboard.setData("text/plain", scope.text)
-      })
-      // FIXME: dirty workaround for ZeroClipboard & Bootstrap Tooltip issue
-      // https://github.com/zeroclipboard/zeroclipboard/issues/369
-      if(element.data('toggle') === 'tooltip') {
-        $('#global-zeroclipboard-html-bridge').tooltip({
-          title: element.attr('title'),
-          placement: element.data('placement')
-        })
-        // disable original title
-        element.removeAttr('title')
+
+      var tooltipOptions = {
+        title: element.attr('title'),
+        placement: element.data('placement')
       }
+
+      client.on('error', function() {
+        // for IE
+        if(window.clipboardData) {
+          element.click(function(e) {
+            window.clipboardData.setData('Text', scope.text) 
+          })
+          element.tooltip(tooltipOptions)
+        } else {
+          element.hide()
+        }
+        ZeroClipboard.destroy()
+      })
+
+      client.on('ready', function() {
+        client.on("copy", function (event) {
+          var clipboard = event.clipboardData;
+          clipboard.setData("text/plain", scope.text)
+        })
+        // FIXME: dirty workaround for ZeroClipboard & Bootstrap Tooltip issue
+        // https://github.com/zeroclipboard/zeroclipboard/issues/369
+        if(element.data('toggle') === 'tooltip') {
+          $('#global-zeroclipboard-html-bridge').tooltip(tooltipOptions)
+          // disable original title
+          element.removeAttr('title')
+        }
+      })
+
     }
   }
 })
